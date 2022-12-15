c3cfdd2d98a12580f2b1cbf7444674cbd38f60c9
Areeb Rajwana <56582057+areebrajwana@users.noreply.github.com>
2022-12-15 15:01:12 +0500

    update
    booking detail page

--- lib/Screens/TabPages/BookingPage/BookingTabs/PreviousTab/bookings_detail_page.dart
+++ lib/Screens/TabPages/BookingPage/BookingTabs/PreviousTab/bookings_detail_page.dart
@@ -21,7 +21,6 @@ class _BookingDetailPageState extends State<BookingDetailPage> {
           title: "Bookings"),
       backgroundColor: homeBgColor,
       body: SingleChildScrollView(
-        // physics: const NeverScrollableScrollPhysics(),
         child: Column(
           children: [
             const BookingsDetailsPrevious(),
@@ -50,92 +49,101 @@ class _BookingDetailPageState extends State<BookingDetailPage> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20),
                 ),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child:
-                            Image.asset('assets/car_bookings_images/close.png'),
-                      ),
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-                    Text(
-                      "Ratings",
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: borderColor),
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                    const Text(
-                      "Give your Ratings \n   and Feedback",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w400,
-                        color: Color(0xffb0b0b0),
-                      ),
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                    Image.asset(
-                      "assets/car_bookings_images/rating.png",
-                      height: 30,
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                    TextField(
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 4,
-                      decoration: InputDecoration(
-                        hintText: "Add your feedback",
-                        hintStyle: const TextStyle(
-                            fontSize: 16, color: Color(0xffb0b0b0)),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide:
-                              const BorderSide(color: Color(0xffd4dce1)),
+                content: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Image.asset(
+                              'assets/car_bookings_images/close.png'),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10, left: 90),
-                      child: Text(
-                        "*Maximum 150 characters",
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.01),
+                      Text(
+                        "Ratings",
                         style: TextStyle(
-                          fontSize: 12,
-                          color: borderColor,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: borderColor),
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02),
+                      const Text(
+                        "Give your Ratings \n   and Feedback",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xffb0b0b0),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 25),
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: Center(
-                          child: Container(
-                            height: 44,
-                            width: 202,
-                            decoration: BoxDecoration(
-                                color: borderColor,
-                                borderRadius: BorderRadius.circular(30)),
-                            child: const Center(
-                              child: Text('Okay',
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontFamily: 'Poppins',
-                                      fontSize: 16)),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02),
+                      Image.asset(
+                        "assets/car_bookings_images/rating.png",
+                        height: 30,
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02),
+                      TextField(
+                        cursorColor: borderColor,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 4,
+                        decoration: InputDecoration(
+                          hintText: "Add your feedback",
+                          hintStyle: const TextStyle(
+                              fontSize: 16, color: Color(0xffb0b0b0)),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(
+                              color: Color(0xffd4dce1),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10, left: 90),
+                        child: Text(
+                          "*Maximum 150 characters",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: borderColor,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 25),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Center(
+                            child: Container(
+                              height: 44,
+                              width: 202,
+                              decoration: BoxDecoration(
+                                  color: borderColor,
+                                  borderRadius: BorderRadius.circular(30)),
+                              child: const Center(
+                                child: Text('Okay',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'Poppins',
+                                        fontSize: 16)),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
