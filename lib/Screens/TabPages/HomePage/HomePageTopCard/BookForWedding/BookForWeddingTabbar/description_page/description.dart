dad76baeab8d2efa38580895890bcaf15f261444
Areeb Rajwana <56582057+areebrajwana@users.noreply.github.com>
2023-01-25 12:38:25 +0500

    update

--- lib/Screens/TabPages/HomePage/HomePageTopCard/BookForWedding/BookForWeddingTabbar/description_page/description.dart
+++ lib/Screens/TabPages/HomePage/HomePageTopCard/BookForWedding/BookForWeddingTabbar/description_page/description.dart
@@ -4,7 +4,7 @@ import '../../../../../../../Utils/api_urls.dart';
 import '../../../../../../../Utils/colors.dart';
 import '../../../../../../../Utils/constants.dart';
 import '../../../../../../../Utils/fontFamily.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 
 class Description extends StatefulWidget {
   const Description({super.key});
@@ -14,7 +14,6 @@ class Description extends StatefulWidget {
 }
 
 class _DescriptionState extends State<Description> {
-
   CarsPhotoGraphyModel carsPhotoGraphyModelObject = CarsPhotoGraphyModel();
   bool loadingP = true;
   @override
@@ -30,17 +29,17 @@ class _DescriptionState extends State<Description> {
       String apiUrl = carsPhotoGraphyApiUrl;
       print("carsPhotographyApi: $apiUrl");
       final response = await http.post(Uri.parse(apiUrl),
-          body: {
-            "users_customers_id" : userId
-          },
+          body: {"users_customers_id": userId},
           headers: {'Accept': 'application/json'});
       print('${response.statusCode}');
       print(response);
       if (response.statusCode == 200) {
         final responseString = response.body;
         print("carsPhotoGraphyResponse: ${responseString.toString()}");
-        carsPhotoGraphyModelObject = carsPhotoGraphyModelFromJson(responseString);
-        print("carsPhotoGraphyObjectLength: ${carsPhotoGraphyModelObject.status}");
+        carsPhotoGraphyModelObject =
+            carsPhotoGraphyModelFromJson(responseString);
+        print(
+            "carsPhotoGraphyObjectLength: ${carsPhotoGraphyModelObject.status}");
       }
     } catch (e) {
       print('Error: ${e.toString()}');
@@ -51,23 +50,27 @@ class _DescriptionState extends State<Description> {
 
   @override
   Widget build(BuildContext context) {
-    return loadingP ? Center(child: CircularProgressIndicator(color: borderColor)) :
-      Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(height: 10),
-        Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20, top: 5),
-          child: Text(
-            "${carsPhotoGraphyModelObject.data![0].description}",
-            style: TextStyle(
-                fontSize: 12,
-                fontFamily: poppinRegular,
-                color: Colors.black),
-            textAlign: TextAlign.left,
-          ),
-        ),
-      ],
-    );
+    return loadingP
+        ? Center(child: CircularProgressIndicator(color: borderColor))
+        : SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 5),
+                  child: Text(
+                    "${carsPhotoGraphyModelObject.data![0].description}",
+                    style: TextStyle(
+                        fontSize: 12,
+                        fontFamily: poppinRegular,
+                        color: Colors.black),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ],
+            ),
+          );
   }
 }
