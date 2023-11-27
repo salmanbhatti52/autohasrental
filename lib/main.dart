b988ef952f329a9be1b2c567c4887dbb346c7123
hammad <hammadraza5388@gmail.com>
2023-11-27 12:29:42 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 import 'Screens/Splash/splash_screen.dart';
 import 'Utils/colors.dart';
-import 'package:device_preview/device_preview.dart';
+// import 'package:device_preview/device_preview.dart';
 
 void main() => runApp(
   // DevicePreview(
@@ -23,8 +23,8 @@ class MyApp extends StatelessWidget {
       builder: (context, orientation, deviceType) {
         return GetMaterialApp(
           useInheritedMediaQuery: true,
-          locale: DevicePreview.locale(context),
-          builder: DevicePreview.appBuilder,
+          // locale: DevicePreview.locale(context),
+          // builder: DevicePreview.appBuilder,
           title: 'AutoHausRental',
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
